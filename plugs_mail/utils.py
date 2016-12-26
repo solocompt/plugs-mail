36455196a6e8e8b9117c240549c7d576f9551468
Ricardo Lobo <ricardolobo@audienciazero.org>
2016-12-26 12:14:36 +0000

    utils are language aware from now on

--- plugs_mail/utils.py
+++ plugs_mail/utils.py
@@ -4,28 +4,28 @@ Plugs Mail Utils
 
 from django.contrib.auth import get_user_model
 
-def to_email(email_class, email, **data):
+def to_email(email_class, email, language=None, **data):
     """
     Send email to specified email address
     """
-    email_class().send([email], **data)
+    email_class().send([email], language=language, **data)
 
-def to_user(email_class, user, **data):
+def to_user(email_class, user, language=None, **data):
     """
     Email user
     """
-    email_class().send([user.email], **data)
+    email_class().send([user.email], language=language, **data)
 
-def to_staff(email_class, **data):
+def to_staff(email_class, language=None, **data):
     """
     Email staff users
     """
     for user in get_user_model().objects.filter(is_staff=True):
-        email_class().send([user.email], **data)
+        email_class().send([user.email], language=language, **data)
 
-def to_superuser(email_class, **data):
+def to_superuser(email_class, language=None, **data):
     """
     Email superusers
     """
     for user in get_user_model().objects.filter(is_superuser=True):
-        email_class().send([user.email], **data)
+        email_class().send([user.email], language=language, **data)
