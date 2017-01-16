34f899f3af748aba667dd3ae0f1e19789c3706c6
Ricardo Lobo <ricardolobo@audienciazero.org>
2017-01-16 21:46:37 +0000

    corrects email utils signatures for utils where we have a user

--- plugs_mail/utils.py
+++ plugs_mail/utils.py
@@ -10,22 +10,22 @@ def to_email(email_class, email, language=None, **data):
     """
     email_class().send([email], language=language, **data)
 
-def to_user(email_class, user, language=None, **data):
+def to_user(email_class, user, **data):
     """
     Email user
     """
-    email_class().send([user.email], language=language, **data)
+    email_class().send([user.email], user.language, **data)
 
-def to_staff(email_class, language=None, **data):
+def to_staff(email_class, **data):
     """
     Email staff users
     """
     for user in get_user_model().objects.filter(is_staff=True):
-        email_class().send([user.email], language=language, **data)
+        email_class().send([user.email], user.language, **data)
 
-def to_superuser(email_class, language=None, **data):
+def to_superuser(email_class, **data):
     """
     Email superusers
     """
     for user in get_user_model().objects.filter(is_superuser=True):
-        email_class().send([user.email], language=language, **data)
+        email_class().send([user.email], user.language, **data)
